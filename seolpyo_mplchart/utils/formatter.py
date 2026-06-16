8472c80e174508f9ed8616c601e2811338e352e3
하얀설표 <white-seolpyo@naver.com>
2026-06-16 15:43:54 +0900

    Add files via upload

--- seolpyo_mplchart/utils/formatter.py
+++ seolpyo_mplchart/utils/formatter.py
@@ -1,4 +1,4 @@
-from .num import num_to_str, unit_data, unit_data_en
+from .num import num_to_str, unit_data, unit_data_en, unit_info_data, unit_info_data_en
 
 
 class Formatter:
@@ -6,6 +6,7 @@ class Formatter:
     volume_word = '주'
 
     unit_data = unit_data
+    unit_info_data = unit_info_data
 
     def price_formatter(self, x, pos, /):
         return num_to_str(x, pos=pos, word=self.price_word, unit_data=self.unit_data)
@@ -13,12 +14,19 @@ class Formatter:
     def volume_formatter(self, x, pos, /):
         return num_to_str(x, pos=pos, word=self.volume_word, unit_data=self.unit_data)
 
+    def info_price_formatter(self, x, pos, /):
+        return num_to_str(x, pos=pos, word=self.price_word, unit_data=self.unit_info_data)
+
+    def info_volume_formatter(self, x, pos, /):
+        return num_to_str(x, pos=pos, word=self.volume_word, unit_data=self.unit_info_data)
+
 
 class FormatterEN(Formatter):
     price_word = '$'
     volume_word = ' Vol.'
 
     unit_data = unit_data_en
+    unit_info_data = unit_info_data_en
 
 FORMATTER = Formatter()
 FORMATTER_EN = FormatterEN()
